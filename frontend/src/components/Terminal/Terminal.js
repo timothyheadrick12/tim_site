b699e3f0540c2a03a85e314c01306fa8a67872ac
Timothy Headrick <twh53r@umsystem.edu>
2021-11-12 10:03:27 -0600

    modified terminal

--- frontend/src/components/Terminal/Terminal.js
+++ frontend/src/components/Terminal/Terminal.js
@@ -1,5 +1,35 @@
 import React, {Component} from 'react'
 import ReactTerminal, {ReactThemes} from 'react-terminal-component'
+import {
+  EmulatorState,
+  OutputFactory,
+  CommandMapping,
+  Outputs,
+  defaultCommandMapping,
+} from 'javascript-terminal'
+
+const customCommandMapping = CommandMapping.create({
+  ...defaultCommandMapping,
+  './boot': {
+    function: () => {
+      return {
+        output: OutputFactory.makeTextOutput('Booting in...'),
+      }
+    },
+    optDef: {},
+  },
+})
+
+const textOutput = OutputFactory.makeTextOutput(
+  "You shouldn't be here... Use './boot' to start the website\n\n"
+)
+
+const customOutputs = Outputs.create([textOutput])
+
+const emulatorState = EmulatorState.create({
+  commandMapping: customCommandMapping,
+  outputs: customOutputs,
+})
 
 export default class Terminal extends Component {
   render() {
@@ -13,6 +43,7 @@ export default class Terminal extends Component {
           })}
           promptSymbol=">"
           clickToFocus="true"
+          emulatorState={emulatorState}
         />
       </div>
     )
